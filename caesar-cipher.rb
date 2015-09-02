98989013de50cd9e5d99f39135a380bd66df6381
Vance <CounterSeal@gmail.com>
2015-09-02 01:44:34 -0700

    Initial commit of substring method.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,4 +1,6 @@
-# Caesar Cipher
+# Caesar Cipher:
+# Implement a caesar cipher that takes in a string and the shift factor
+# and then outputs the modified string
 
 def caesarSalad(text, num)
 	alphabet = ('a'..'z').to_a
