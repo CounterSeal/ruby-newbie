4b87676e3d3031bb2f95087005f5731a131ba394
CounterSeal <CounterSeal@gmail.com>
2015-09-12 21:35:33 -0700

    Bubble Sort complete; first-pass

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -11,16 +11,75 @@
 # Use this to sort your array.
 
 def bubble_sort(nums)
-	swapCount = 0
+	swapped = true
 
-	nums.map_with_index do |number, i|
-		next if (i + 1 == nums.size)
-		if (number > nums[i+1])
-			number, nums[i+1] == nums[i+1], number
-			swapCount += 1
+	while swapped
+		swapped = false
+		for i in (0...nums.size - 1)
+			if (nums[i] > nums[i+1])
+				nums[i], nums[i+1] = nums[i+1], nums[i]
+				swapped = true
+			end
 		end
 	end
+
+	p nums
 end
 
-
 bubble_sort([4,3,78,2,0,2]) #[0,2,2,3,4,78]
+bubble_sort([4,7,10,15,11,4]) #[4,4,7,10,11,15]
+bubble_sort([4,8,4]) #[4,4,8]
+bubble_sort([99,71]) #[71,99]
+bubble_sort([8,17]) #[8,17]
+bubble_sort([2]) #[2]
+bubble_sort([100,65,44,21,19,18,12,7,3]) #[3,7,12,18,19,21,44,65,100]
+
+
+def bubble_sort_by(sortThis)
+	swapped = true
+
+	while swapped
+		swapped = false
+		for i in (0...sortThis.size - 1)
+			y = yield(sortThis[i], sortThis[i+1])
+			if (y >= 1)
+				sortThis[i], sortThis[i+1] = sortThis[i+1], sortThis[i]
+				swapped = true
+			end
+		end
+	end
+
+	p sortThis
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length - right.length
+end #["hi", "hey", "hello"]
+
+bubble_sort_by([4,3,78,2,0,2]) do |left,right|
+	left - right
+end #[0,2,2,3,4,78]
+
+bubble_sort_by([4,7,10,15,11,4]) do |left,right|
+	left - right
+end #[4,4,7,10,11,15]
+
+bubble_sort_by([4,8,4]) do |left,right|
+	left - right
+end #[4,4,8]
+
+bubble_sort_by([99,71]) do |left,right|
+	left - right
+end #[71,99]
+
+bubble_sort_by([8,17]) do |left,right|
+	left - right
+end #[8,17]
+
+bubble_sort_by([2]) do |left,right|
+	left - right
+end #[2]
+
+bubble_sort_by([100,65,44,21,19,18,12,7,3]) do |left,right|
+	left - right
+end #[3,7,12,18,19,21,44,65,100]
