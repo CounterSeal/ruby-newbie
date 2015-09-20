2526c09bb6aa3384f904c5ddc40c665aff898c76
CounterSeal <CounterSeal@gmail.com>
2015-09-20 00:00:25 -0700

    Enumerables complete

--- enumerables.rb
+++ enumerables.rb
@@ -61,6 +61,59 @@ module Enumerable
 		true
 	end
 
+	def my_count(n = nil)
+		counter = 0
+		if block_given?
+			for i in self
+				counter += 1 if yield(i)
+			end
+		elsif n.nil?
+			for i in self
+				counter += 1
+			end
+		else
+			for i in self
+				counter += 1 if (i == n)
+			end
+		end
+		counter
+	end
+
+	def my_map(p = nil)
+		result = []
+		if p && block_given?
+			for i in self
+				result << p.call(yield(i))
+			end
+		elsif p
+			for i in self
+				result << p.call(i)
+			end
+		elsif block_given?
+			for i in self
+				result << yield(i)
+			end
+		else
+			result = self.to_enum(:my_map)
+		end
+		result
+	end
+
+	def my_inject(n = nil)
+		if n.nil?
+			result = self[0]
+			for i in self[1..(self.size)]
+				result = yield(result,i)
+			end
+		else
+			result = n
+			for i in self[0..(self.size)]
+				result = yield(result,i)
+			end
+		end
+		result
+	end
+
 end
 
 a = [1,2,3,4,5,6,7]
@@ -76,8 +129,8 @@ d = []
 #b.my_each_with_index {|word, i| puts "#{i}. #{word}!"}
 
 # my_select
-#puts a.my_select {|n| n % 2 == 0}
-#puts b.my_select {|word| word[0].downcase == "h"}
+#p a.my_select {|n| n % 2 == 0}
+#p b.my_select {|word| word[0].downcase == "h"}
 
 # my_all?
 #puts a.my_all? {|n| n < 8} #true
@@ -96,9 +149,32 @@ d = []
 #puts d.my_any? {|n| n < 2} #false
 
 # my_none?
-puts a.my_none? {|n| n > 8} #true
-puts b.my_none? {|word| word.length <= 3} #true
-puts d.my_none? #true
-puts d.my_none? {|n| n < 2} #true
-puts b.my_none? {|word| word[0] == "S"} #false
-puts c.my_none? #false
+#puts a.my_none? {|n| n > 8} #true
+#puts b.my_none? {|word| word.length <= 3} #true
+#puts d.my_none? #true
+#puts d.my_none? {|n| n < 2} #true
+#puts b.my_none? {|word| word[0] == "S"} #false
+#puts c.my_none? #false
+
+# my_count
+#puts a.my_count #7
+#puts b.my_count #4
+#puts c.my_count #5
+#puts d.my_count #0
+#puts a.my_count(5) #1
+#puts a.my_count {|n| n%3 == 0} #2
+
+# my_map
+#p a.my_map {|n| n*2} #[2,4,6,8,10,12,14]
+#p b.my_map #enum
+#p b.my_map {|x| x + ", Replicant!"} #["Hello, Replicant!","Ni Hao, Replicant!","Salut, Replicant!","Hola, Replicant!"]
+#p c.my_map {"doge"} #["doge", "doge", "doge", "doge", "doge"]
+#p d.map #enum
+testProc = Proc.new {|n| n*2}
+p a.my_map(testProc) #[2,4,6,8,10,12,14]
+
+# my_inject
+#puts a.my_inject(3) {|sum, n| sum + n} #28
+#puts a.my_inject {|product, n| product * n} #5040
+#puts a.my_inject(2) {|product, n| product * n} #10080
+#puts b.my_inject {|memo, word| memo.length > word.length ? memo : word} #Ni Hao
