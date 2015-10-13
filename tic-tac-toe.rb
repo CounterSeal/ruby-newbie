df8b5f0f57cf005f5a5f2c87d582653ab06e58c6
CounterSeal <CounterSeal@gmail.com>
2015-10-12 23:24:17 -0700

    Board class progress

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -10,6 +10,19 @@ module TicTacToe
 		end
 	end
 
+	class Board
+		attr_reader :grid
+		def initialize(input = {})
+			@grid = input.fetch(:grid, default_grid)
+		end
+
+		private
+
+		def default_grid
+			Array.new(3) { Array.new(3) { Cell.new } }
+		end
+	end
+
 	class Player
 		attr_accessor :mark, :name
 		def initialize(input)
