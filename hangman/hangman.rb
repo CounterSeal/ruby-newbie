783a34ae8ff76d0c6701f70fc4986e61c0f09aa4
Vance <CounterSeal@gmail.com>
2015-12-13 18:34:20 -0800

    Hangman progress.

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -2,19 +2,20 @@
 
 module Hangman
 	class Game
-		attr_accessor :game, :answer, :board, :guess
+		attr_accessor :game, :answer, :board, :guess, :guessed
 		def initialize
 			@game = game
 			@answer = answer
 			@board = board
 			@guess = guess
+			@guessed = []
 		end
 
 		def start
 			puts "Welcome to Hangman!"
 			@answer = word.downcase
 			@board = create_board
-			puts "Answer: #{@answer}"
+			puts "[Debug] Answer: #{@answer}"
 			puts @board.join
 			play
 		end
@@ -26,17 +27,23 @@ module Hangman
 			guesses_left = 5
 			6.times do
 				@guess = gets.chomp.downcase
+
 				if @guess == @answer
 					puts "Correct. You win!"
-					break
+					exit
+				else
+					guess_process(@guess)
+					if @board.join == @answer
+						puts "Correct. You win!"
+						exit
+					else
+						puts @board.join
+						puts "#{guesses_left} guesses remaining."
+						puts "Incorrect guesses: #{@guessed.join(', ')}"
+						guesses_left -= 1
+					end
 				end
-
-				guess_process(@guess)
-				puts @board.join
-				puts "#{guesses_left} guesses remaining."
-				guesses_left -= 1
 			end
-
 		end
 
 		def guess_process(player_guess)
@@ -47,6 +54,7 @@ module Hangman
 					end
 				end
 			else
+				@guessed << player_guess
 				puts "Try Again!"
 			end
 		end
