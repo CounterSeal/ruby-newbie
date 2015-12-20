45b3f9a2ed0287620b8ea0f571be52cf1a5dc69f
CounterSeal <CounterSeal@gmail.com>
2015-12-19 22:16:38 -0800

    Basic functionality complete.

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -4,30 +4,57 @@ require 'open-uri'
 
 module Hangman
 	class Game
-		attr_accessor :game, :answer, :board, :guess, :guessed, :guesses_left
+		attr_accessor :game, :answer, :board, :guess, :guessed, :guesses_left, :visual
 		def initialize
 			@game = game
 			@answer = answer
 			@board = board
 			@guess = guess
 			@guessed = []
-			@guesses_left = 5
+			@guesses_left = 6
+			@visual = ["____", "|  |", "|", "|", "|"]
 		end
 
 		def start
 			puts "Welcome to Hangman!"
-			@answer = word.downcase
-			@board = create_board
-			puts "[Debug] Answer: #{@answer}"
-			puts @board.join
-			play
+			puts "Enter \"-save\" at any time during the game to save your progress."
+
+			while true
+				puts "Would you like to (1) Start a new game or (2) Load your last saved game?"
+				choice = gets.chomp
+
+				case choice
+				when "1"
+					@answer = word.downcase
+					@board = create_board
+					#puts "[Debug] Answer: #{@answer}"
+					visual_board
+					puts @board.join
+					play
+					break
+				when "2"
+					load_save
+					@answer
+					@board
+					#puts "[Debug] Answer: #{@answer}"
+					visual_board
+					puts @board.join
+					puts "#{guesses_left} guesses remaining."
+					puts "Incorrect guesses: #{@guessed.join(', ')}"
+					play
+					break
+				else
+					puts "Please enter 1 or 2."
+				end
+			end
 		end
 
 		private
 
 		def play
 			puts "Take your guess!"
-			6.times do
+			puts "\n"
+			while @guesses_left > 0
 				@guess = gets.chomp.downcase
 				commands
 				if @guess == @answer
@@ -40,13 +67,16 @@ module Hangman
 						puts "Correct. You win!"
 						exit
 					elsif @guesses_left == 0
+						visual_board
 						puts "You lose!"
+						puts "The correct word was \"#{@answer}\"."
+						exit
 					else
+						visual_board
 						puts @board.join
 						puts "#{guesses_left} guesses remaining."
 						puts "Incorrect guesses: #{@guessed.join(', ')}"
 						puts "\n"
-						@guesses_left -= 1
 					end
 				end
 			end
@@ -61,6 +91,7 @@ module Hangman
 				end
 			else
 				@guessed << player_guess
+				@guesses_left -= 1
 			end
 		end
 
@@ -78,11 +109,21 @@ module Hangman
 			end
 		end
 
+		def load_save
+			contents = File.open("save_game.txt", "r").readlines
+
+			@answer = contents[0].chomp
+			@board = contents[1].chomp.split('')
+			@guesses_left = contents[2].chomp.to_i
+			@visual = contents[3].split('-')
+			@guessed = contents[4].chomp.split('') unless contents[3].chomp.empty?
+		end
+
 		def save
-			saved_data = { answer: @answer, board: @board, guesses_left: @guesses_left + 1, guessed: @guessed }
-			save_file = File.open("save_game.txt", "w")
-			save_file.puts saved_data.to_s
-			save_file.close
+			saved_data = [@answer, @board.join, @guesses_left + 1, @visual.join('-'), @guessed.join]
+			File.open("save_game.txt", "w"){ |line| line.puts(saved_data) }
+			puts "Game saved!"
+			puts "\n"
 		end
 
 		def define
@@ -99,6 +140,31 @@ module Hangman
 		def create_board
 			board = '-' * @answer.length
 			board.split('')
+		end
+
+		def visual_board
+			case @guesses_left
+			when 6
+				puts @visual
+			when 5
+				@visual[2] = "|  O"
+				puts @visual
+			when 4
+				@visual[3] = "|  |"
+				puts @visual
+			when 3
+				@visual[3] = "| /|"
+				puts @visual
+			when 2
+				@visual[3] = "| /|\\"
+				puts @visual
+			when 1
+				@visual[4] = "| /"
+				puts @visual
+			when 0
+				@visual[4] = "| / \\"
+				puts @visual
+			end
 		end
 	end
 end
