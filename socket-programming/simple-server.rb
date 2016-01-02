30952c040b2ac45f599a1431f482583ad4cb3ec4
Vance <CounterSeal@gmail.com>
2016-01-02 14:37:52 -0800

    First pass complete. Needs some polish.

--- socket-programming/simple-server.rb
+++ socket-programming/simple-server.rb
@@ -1,19 +1,47 @@
 # A Simple Server
 
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000)
 loop do
 	client = server.accept
+=begin
 	request = client.gets
-	method = request.split(' ')[0]
+	header = request.split("\r\n\r\n")[0]
+	method,path,version = header.split(' ')
+	file = File.open(path)
+=end
+	
+	header = ""
+	while line = client.gets
+		header += line
+		break if header =~/\r\n\r\n$/
+	end
+
+	method = header.split[0]
+  path = header.split[1][1..-1]
+  version = header.split[2]
+
+  file = File.open(path)
+
+	body_size = header.split(' ').last.to_i
+	body = client.read(body_size)
 
 	if method == "GET"
-		method,path,version = request.split(' ')
 		client.puts(version + " 200 OK")
 		client.puts("Current time: " + Time.now.ctime)
-		client.puts("File length: " + File.open(path).size.to_s + "\r\n\r\n")
-		client.puts(File.open(path).readlines.each { |line| puts line })
+		client.puts("Content-Length: " + file.size.to_s + "\r\n\r\n")
+		client.puts(file.readlines.each { |line| puts line })
+	elsif method == "POST"
+		params = JSON.parse(body)
+		data = "<li>Name: #{params['viking']['name']}</li>\n\t\t\t" +
+					 "<li>E-Mail: #{params['viking']['email']}</li>"
+
+		client.puts("POST #{path} #{version} 200 OK\n" +
+							  "#{Time.now.ctime}\n" +
+								"Content-Length: #{file.size}\r\n\r\n" +
+								"#{file.read.gsub('<%= yield %>', data)}")
 	else
 		client.puts(version + " 404 Not Found")
 		client.puts(Time.now.ctime)
--- socket-programming/tiny-browser.rb
+++ socket-programming/tiny-browser.rb
@@ -1,10 +1,11 @@
 # A Tiny Web Browser
 
 require 'socket'
+require 'json'
 
 host = 'localhost'
 port = 2000
-path = "index.html"
+path = "/index.html"
 
 puts "What type of request would you like to send?"
 while true
@@ -13,12 +14,16 @@ while true
 		request = "GET #{path} HTTP/1.0\r\n\r\n"
 		break
 	elsif request_type == "POST"
-		request = "POST #{path} HTTP/1.0\r\n\r\n"
 		printf "What is your name? "
 		name = gets.chomp
 		printf "What is your e-mail address? "
 		email = gets.chomp
 		vikings = {:viking => {:name => name, :email => email}}
+		json_hash = vikings.to_json
+
+		request = "POST /thanks.html HTTP/1.0\n" +
+		"Content-Length: #{json_hash.length}\r\n\r\n" +
+		"#{json_hash}"
 		break
 	else
 		puts "Invalid request. Enter either GET or POST"
@@ -30,10 +35,11 @@ socket.print(request)
 response = socket.read
 headers,body = response.split("\r\n\r\n", 2)
 
-status = headers.split("\n")[0]
-version,code,reason = status.split(' ')
-if code == "200"
-	puts body
-else
-	puts code + ' ' + reason
-end
+puts body
+#status = headers.split("\n")[0]
+#version,code,reason = status.split(' ')
+#if code == "200"
+	#puts body
+#else
+	#puts code + ' ' + reason
+#end
