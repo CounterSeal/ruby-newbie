e69342c217d48ff57838afb48dbc465702b6e674
CounterSeal <CounterSeal@gmail.com>
2016-03-06 21:17:31 -0800

    Part 2 buggy. Day 03 Part 2 progress.

--- advent-of-code/day-03.rb
+++ advent-of-code/day-03.rb
@@ -2,85 +2,92 @@
 # http://adventofcode.com/
 # Day 3
 
-# Part One
+# Part One and Part Two
+$count = 1
+$grid = [[1]]
+
 def present_houses(instructions)
-	count = 1
-	grid = [[1]]
 	y, x = 0, 0
 
 	instructions.split('').each do |direction|
 		case direction
 		when "^"
 			if y == 0
-				grid_width = grid.max_by { |row| row.length }.length
+				grid_width = $grid.max_by { |row| row.length }.length
 				new_row = Array.new(grid_width)
 				new_row[x] = 1
-				grid.unshift(new_row)
-				count += 1
+				$grid.unshift(new_row)
+				$count += 1
 			else
-				if grid[y-1][x] == nil
-					grid[y-1][x] = 1
-					count += 1
+				if $grid[y-1][x] == nil
+					$grid[y-1][x] = 1
+					$count += 1
 					y -= 1
 				else
-					grid[y-1][x] += 1
+					$grid[y-1][x] += 1
 					y -= 1
 				end
 			end
 		when ">"
-			if grid[y][x+1] == nil
-				grid[y][x+1] = 1
-				count += 1
+			if $grid[y][x+1] == nil
+				$grid[y][x+1] = 1
+				$count += 1
 				x += 1
 			else
-				grid[y][x+1] += 1
+				$grid[y][x+1] += 1
 				x += 1
 			end
 		when "v"
-			if y == grid.length-1
-				grid_width = grid.max_by { |row| row.length }.length
+			if y == $grid.length-1
+				grid_width = $grid.max_by { |row| row.length }.length
 				new_row = Array.new(grid_width)
 				new_row[x] = 1
-				grid << new_row
-				count += 1
+				$grid << new_row
+				$count += 1
 				y += 1
 			else
-				if grid[y+1][x] == nil
-					grid[y+1][x] = 1
-					count += 1
+				if $grid[y+1][x] == nil
+					$grid[y+1][x] = 1
+					$count += 1
 					y += 1
 				else
-					grid[y+1][x] += 1
+					$grid[y+1][x] += 1
 					y += 1
 				end
 			end
 		when "<"
 			if x == 0
-				grid.each { |row| row.unshift(nil) }
-				grid[y][x] = 1
-				count += 1
+				$grid.each { |row| row.unshift(nil) }
+				$grid[y][x] = 1
+				$count += 1
 			else
-				if grid[y][x-1] == nil
-					grid[y][x-1] = 1
-					count += 1
+				if $grid[y][x-1] == nil
+					$grid[y][x-1] = 1
+					$count += 1
 					x -= 1
 				else
-					grid[y][x-1] += 1
+					$grid[y][x-1] += 1
 					x -= 1
 				end
 			end
 		end
 	end
 
-	count
+	#$count
 	#puts count
 	#grid.each { | line| puts line.to_s }
 end
 
-# Part Two
 
+#puzzle = File.open("day-03-puzzle.txt", "r") { |file| file.read }
+puzzle = "^v"
 
+puzzle_arr = puzzle.split('')
+santa = puzzle_arr.values_at(* puzzle_arr.each_index.select { |i| i.even? }).join('')
+robo_santa = puzzle_arr.values_at(* puzzle_arr.each_index.select { |i| i.odd? }).join('')
 
-puzzle = File.open("day-03-puzzle.txt", "r") { |file| file.read }
+#puts present_houses(puzzle)
+present_houses(santa)
+present_houses(robo_santa)
 
-puts present_houses(puzzle)
+puts $count
