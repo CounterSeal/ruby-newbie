97ce4111ecf9227fe1e31700511631f3d4b4230a
CounterSeal <CounterSeal@gmail.com>
2016-04-08 00:25:54 -0700

    Connect Four progress.

--- connect-four.rb
+++ connect-four.rb
@@ -10,10 +10,78 @@ module ConnectFour
 		end
 
 		def start
+			while true
+				get_move
+
+			end
+		end
+
+		def get_move
+			while true
+				human_move = gets.upcase.chomp
+				x, y = human_move_to_coordinate(human_move)
+				if human_move.length < 3
+					if board.get_cell(x, y).value == player1.mark || board.get_cell(x, y).value == player2.mark
+						puts "That move is taken! Choose another."
+					else
+						return x, y
+					end
+				else
+					puts "Enter an appropriate letter and number combination."
+				end
+			end
 		end
 
 		def switch_players
 			@player1, @player2 = @player2, @player1
+		end
+
+		def human_move_to_coordinate(human_move)
+			mapping = {
+				"A1" => [0, 0],
+				"A2" => [1, 0],
+				"A3" => [2, 0],
+				"A4" => [3, 0],
+				"A5" => [4, 0],
+				"A6" => [5, 0],
+				"B1" => [0, 1],
+				"B2" => [1, 1],
+				"B3" => [2, 1],
+				"B4" => [3, 1],
+				"B5" => [4, 1],
+				"B6" => [5, 1],
+				"C1" => [0, 2],
+				"C2" => [1, 2],
+				"C3" => [2, 2],
+				"C4" => [3, 2],
+				"C5" => [4, 2],
+				"C6" => [5, 2],
+				"D1" => [0, 3],
+				"D2" => [1, 3],
+				"D3" => [2, 3],
+				"D4" => [3, 3],
+				"D5" => [4, 3],
+				"D6" => [5, 3],
+				"E1" => [0, 4],
+				"E2" => [1, 4],
+				"E3" => [2, 4],
+				"E4" => [3, 4],
+				"E5" => [4, 4],
+				"E6" => [5, 4],
+				"F1" => [0, 5],
+				"F2" => [1, 5],
+				"F3" => [2, 5],
+				"F4" => [3, 5],
+				"F5" => [4, 5],
+				"F6" => [5, 5],
+				"G1" => [0, 6],
+				"G2" => [1, 6],
+				"G3" => [2, 6],
+				"G4" => [3, 6],
+				"G5" => [4, 6],
+				"G6" => [5, 6]
+			}
+			mapping[human_move]
 		end
 	end
 
